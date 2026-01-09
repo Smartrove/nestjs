bb95a99677017d25b3c7e437d3150842fa727ab7
smartrove <olaokunolalekan@gmail.com>
2026-01-09 21:10:21 +0100

    login logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from './create-user-dto';
+import { UsersService } from './users.service';
+import { LoginUserDTO } from './login-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post('/create')
-  create(
+  async create(
     @Body()
     createUserDTO: CreateUserDTO,
   ) {
-    return createUserDTO;
+    return await this.usersService.signup(createUserDTO);
+  }
+  @Post('/login')
+  async login(
+    @Body()
+    loginUserDTO: LoginUserDTO,
+  ) {
+    return await this.usersService.login(loginUserDTO);
   }
 }
